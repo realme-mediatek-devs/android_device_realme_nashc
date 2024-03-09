f75f90fa7aa1bba914c8a64ff379d73d1dfeab9b
bgcngm <bgcngm@gmail.com>
2024-03-09 10:26:23 +1000

    nashc: Migrate to AIDL ClearKey DRM HAL

--- device.mk
+++ device.mk
@@ -109,13 +109,14 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor
 
 # Display
 PRODUCT_PACKAGES += \

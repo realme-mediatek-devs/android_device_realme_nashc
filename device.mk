fe1c5d737f5b117d4d6ac1c1b63747878026dbe3
Fastle <131449950+Fastlens@users.noreply.github.com>
2024-04-20 04:17:24 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -405,6 +405,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/mediatek \
     hardware/oplus
 

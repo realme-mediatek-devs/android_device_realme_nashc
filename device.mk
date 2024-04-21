97256e4b9e775c44b3793eb60ad4ba50aefafab0
Fastlens <fastledx@gmail.com>
2024-04-21 22:22:35 +0000

    backup

--- device.mk
+++ device.mk
@@ -405,8 +405,6 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/mediatek \
     hardware/oplus
 

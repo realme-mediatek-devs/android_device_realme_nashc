c240bbce497ac564281883675e5f3238a964c82c
Fastle <131449950+Fastlens@users.noreply.github.com>
2024-04-21 06:37:52 +0300

    nashc: Add DT2W support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,9 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi
 TARGET_BOARD_PLATFORM := mt6785
 BOARD_HAS_MTK_HARDWARE := true
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
